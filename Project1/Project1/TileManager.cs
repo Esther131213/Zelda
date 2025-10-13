1748b30074fd2c09165a22a09c31b82084ab56fa
Esther131213 <estherlimalmgren@gmail.com>
2025-10-13 10:33:14 +0200

    starting to work on the tile manager

--- Project1/Project1/TileManager.cs
+++ Project1/Project1/TileManager.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Zelda
 {
     internal class TileManager
     {
+        int[,] tileMap;
+
+        Texture2D tileset;
+        int tileSize = 32;
+
+        void LoadTileMap(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            int rows = lines.Length;
+            int cols = lines[0].Split(",").Length;
+
+            tileMap = new int[rows, cols];
+
+            for (int y = 0; y < rows; y++)
+            {
+                var tiles = lines[y].Split(",");
+                for (int x = 0; x < cols; x++)
+                {
+                    tileMap[x,y] = int.Parse(tiles[x]);
+                }
+            }
+        }
+
+        void Draw(SpriteBatch sp)
+        {
+
+        }
     }
 }
